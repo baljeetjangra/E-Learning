bb561623a9711c5fb2fd0fbad8c43e16bbf197cd
Baljeet Jangra <baljeetjangra45@gmail.com>
2020-09-28 11:03:51 +0530

    final changes

--- elearning/settings.py
+++ elearning/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import django_heroku 
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -167,3 +167,5 @@ STATICFILES_DIRS = [
 ]
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
+
+django_heroku.settings(locals())
