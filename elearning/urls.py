94277f47cd8b81391c6d00822d91190d2de9b2a2
Baljeet Jangra <baljeetjangra45@gmail.com>
2020-09-28 10:07:02 +0530

    make changes for  heroku

--- elearning/urls.py
+++ elearning/urls.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.static import serve
+from django.conf.urls import url
 
 
 admin.site.site_header = "Rishabh Notes"
@@ -13,6 +15,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('standard.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
 
 if settings.DEBUG:
